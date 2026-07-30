b77d1c6004dc5b4998a2e607a2c57c03c8f770b3
Alice Calvert <dallas6vert@gmail.com>
2026-07-29 20:56:59 -0400

    Add files via upload

--- Absorption_Simulation.m
+++ Absorption_Simulation.m
@@ -7,15 +7,15 @@
 %% nanoparticles are immersed, the range of wavelengths, and the option of whether to display the alpha values in the command window or not.
 %% The outputs are the absorption coefficients at each wavelength.
 %% The simulation is based off algorithm by Dani et al. [1]
-%% All units are SI except the absorption coefficient (cm^-1). Angles are in rads.
+%% All units are SI except the absorption coefficient [cm^-1]. Angles are in rads.
 
 %% -------------------------------------------------------------------------- %%
 %% ------------------------------- References ------------------------------- %%
 %% -------------------------------------------------------------------------- %%
-%% [2] R.K. Dani et al., "Supplemental Material for 'Faraday rotation enhancement 
+%% [1] R.K. Dani et al., "Supplemental Material for 'Faraday rotation enhancement 
 %% of gold coated Fe2O3 nanoparticles: Comparison of experiment and theory' "
 %%     J. Chem. Phys, vol. 135, no. 224502, 2011. 
-%% [3] A. Ibrahim, “Synthesis and Characterization of Magnetic Nanoparticles 
+%% [2] A. Ibrahim, “Synthesis and Characterization of Magnetic Nanoparticles 
 %%     to Incorporate into Silicon Waveguides to be Used as Optical Isolators,” 
 %%     M.S. thesis, Eng. Phys., McMaster Univ., Hamilton, Ontario, 2019. [Online]. Available: https://macsphere.mcmaster.ca/bitstream/11375/24720/2/Ibrahim_Amr_E_201908_MASc.pdf 
 
@@ -30,8 +30,8 @@ tic %start timing run
 %% General Parameters & Constants
 
 c=3e8;                             % speed of light [m/s]
-Bz=4.2;                            % applied magnetic field along z [T]
-B=4.2;                             % applied magnetic field [T]
+Bz=0;                              % applied magnetic field along z [T]
+B=0;                               % applied magnetic field [T]
 T=20+273.15;                       % temperature [K]
 kb=1.38064852e-23;                 % Boltzmann constant
 me=9.10938356e-31;                 % effective mass of electron [kg]
@@ -49,7 +49,7 @@ absorption = zeros(size(wavelength));                     % variable that will s
 %% -------------------------- Parameters of shell --------------------------- %%
 
 %% Gold parameters (fitted using 17 nm diameter gold NPs in water; interband transitions are neglected)
-% Based off Drude-Sommerfeld Theory [2]
+% Based off Drude-Sommerfeld Theory [1]
 
 g_tau=9.1e-15;                     % scattering time 
 vf =1.4e6;                         % Fermi velocity
@@ -68,28 +68,28 @@ epsa=eff_medium;
 
 if strcmpi(core_material, 'sno2')
 
-    %% Option 1: SnO2 [3]
+    %% Option 1: SnO2 [2]
 
-    Keff=5e3;                                   % effective anisotropy constant, 5-9e3 [3] J/m^2
-    Ms=250e3;                                   % saturation magnetization, 250-300e3 [3] A/m
+    Keff=5e3;                                   % effective anisotropy constant, 5-9e3 [2] J/m^2
+    Ms=250e3;                                   % saturation magnetization, 250-300e3 [2] A/m
     c_wp=0;                                     % plasma frequency [rad/s]
     c_gammap = (2.75e14/(0.347e-15))+vf/b;      % damping frequency, CHANGE
     c_g0=1.2e15;                                % fitted parameter for tin oxide absorption 
     c_w0=6.7e15;                                % fitted parameter for tin oxide absorption 
     c_gamma0=9e15;                              % fitted parameter for tin oxide absorption  
-    c_ns=2.65e5;%8.3893e+20                     % number density [1/m^3]
+    c_ns=2.65e5;                                % number density [1/m^3]
 
 elseif strcmpi(core_material, 'fe2o3')
     
-    %% Option 2: Fe2O3 [2]
+    %% Option 2: Fe2O3 [1]
 
     Keff=4700;%9e3;                           % effective anisotropy constant, 5-9e3 J/m^2
     Ms=414e3;%250e3;                          % saturation magnetization, 250-300e3 A/m
     c_wp=0;                                   % plasma frequency [rad/s]
     c_gammap=1/(0.347e-15)+vf/b;              % damping frequency
-    c_g0=5.2e15;                              % fitted parameter for iron oxide absorption [2]
-    c_w0=5.06e15;                             % fitted parameter for iron oxide absorption [2]
-    c_gamma0=2.89e15;                         % fitted parameter for iron oxide absorption [2]
+    c_g0=5.2e15;                              % fitted parameter for iron oxide absorption [1]
+    c_w0=5.06e15;                             % fitted parameter for iron oxide absorption [1]
+    c_gamma0=2.89e15;                         % fitted parameter for iron oxide absorption [1]
     c_ns=3.5e18;                              % number density [1/m^3]
 
 elseif strcmpi(core_material, 'other')
@@ -128,7 +128,7 @@ elseif strcmpi(core_material, 'other')
 
 end
 
-c_ns = planar_density/(2*a);                % number density [1/m^3]
+c_ns = planar_density/1;                    % number density [1/m^3] = planar density [1/m^2] / 1 m
 Bzint=(((2/9)*mu0*c_Vs*Ms^2)/(kb*T))*B;     % internal magnetic field
 c_wB=(e*Bzint)/(me);                        % cyclotron frequency, assuming bulk effective mass of 9.5me^2
 fs=c_ns*Vs;                                 % volume fraction
@@ -152,7 +152,7 @@ for i = 1:length(wavelength)
     eps_cR= 1-(c_g0^2)/(w^2-c_w0^2+1i*c_gamma0*w+w*c_wB); % dielectric function, metal oxide, right polarization
     
     % ------------------ Core/shell permittivity (eps_s) --------------------- %
-    % Based off Maxwell-Garnet Theory, with shell as effective medium [2]
+    % Based off Maxwell-Garnet Theory, with shell as effective medium [1]
  
     beta_cL=fc*(eps_cL-eps_bL)/(eps_cL+2*eps_bL);
     beta_cR=fc*(eps_cR-eps_bR)/(eps_cR+2*eps_bR);
@@ -161,7 +161,7 @@ for i = 1:length(wavelength)
 
    
     % ----------------- Effective permittivity (eps_final) ------------------- %
-    % Based off Maxwell-Garnet Theory, with water medium [2]
+    % Based off Maxwell-Garnet Theory, with water medium [1]
 
     beta_sL = fs*(eps_sL-epsa)/(eps_sL+2*epsa);
     beta_sR = fs*(eps_sR-epsa)/(eps_sR+2*epsa);
